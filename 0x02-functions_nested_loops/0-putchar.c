ba1953dd860e14f6851640a4311f584865bd586d
barhad <barhadbarhad001@gmail.com>
2023-03-21 11:02:27 -0700

    add : 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
-#include "putchar.c"
+#include "main.h"
 
 /**
- * main - Prints Putchar as a message.
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
+ *
  */
-int main.h(void)
-{
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
