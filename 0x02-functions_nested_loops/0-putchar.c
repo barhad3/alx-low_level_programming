0654a265cf243da1b614add155b70456b2a4ce84
barhad <barhadbarhad001@gmail.com>
2023-03-21 10:56:22 -0700

    add : 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,20 @@
 #include "putchar.c"
 
 /**
- * main - Prints putchar
- * @void: Empty parameter list for main.
+ * main - Prints Putchar as a message.
  *
- * Description: Prints Holberton w/out stdio
- *
- * Return: 0 for success
-*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
 	_putchar('\n');
 	return (0);
 }
