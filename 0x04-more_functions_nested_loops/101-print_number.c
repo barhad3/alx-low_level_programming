746f0d0f6d8e520358effbc0e824063e9eb500ec
barhad <barhadbarhad001@gmail.com>
2023-03-23 03:46:07 -0700

    add : all

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,13 +5,14 @@
  * @n: input integer.
  * Return: no return.
  */
-void print_number(int n) {
+void print_number(int n)
+{
     if (n < 0) {
         _putchar('-');
         n = -n;
     }
-    if (n / 10) {
+    if (n / 10 != 0) {
         print_number(n / 10);
     }
-    _putchar(n % 10 + '0');
+    _putchar((n % 10) + '0');
 }
