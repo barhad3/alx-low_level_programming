0a2205b2b4f57698788606ec944c8372518e7bf6
barhad <barhadbarhad001@gmail.com>
2023-04-05 02:48:02 -0700

    add : all

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,23 +6,17 @@
  * which can replace any string (including an empty string).
  * @s1: the first string to compare
  * @s2: the second string to compare (can contain '*')
- *
  * Return: 1 if the strings can be considered identical, 0 otherwise.
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '\0')
-		return (*s1 == '\0');
-
-	if (*s2 == '*') {
-		if (*s1 == '\0')
-			return (wildcmp(s1, s2 + 1));
-
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-	}
-
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	return (0);
 }
