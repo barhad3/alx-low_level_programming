39617df29482730ae12ffae643c651efc56b953d
barhad <barhadbarhad001@gmail.com>
2023-04-05 02:26:12 -0700

    add : all

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,7 +3,6 @@
 /**
  * factorial - returns the factorial of a given number
  * @n: the number to calculate the factorial of
- *
  * Return: the factorial of n, or -1 if n is negative
  */
 int factorial(int n)
